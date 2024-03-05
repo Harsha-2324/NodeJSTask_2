d8c6c8d27ec978516e07fd2ecf0252ab53e99afd
Harsha-2324 <rharshva@gmail.com>
2024-03-06 00:22:22 +0530

    Hall Booking Commit

--- src/controller/index.js
+++ src/controller/index.js
@@ -3,11 +3,11 @@ const homePage = (req, res) => {
      <div>
      <h1 style="background-color:purple; text-align: center">🌟NodeJS_Hall_Booking_API_Task🎊</h1>
      <ul>
-        <li><h2><span style="background-color:yellow">GET</span>: Get all Rooms use endpoint: /rooms</h2></li>
-        <li><h2><span style="background-color:yellow">GET</span>: Take a list of Booked rooms use endpoint: /rooms/customer</h2></li>
-        <li><h2><span style="background-color:green">POST</span>: Create a new room use endpoint: /rooms/</h2></li>
-        <li><h2><span style="background-color:lightblue">PUT</span>: New Hall Booking use endpoint: /rooms/:id </h2></li>
-        <li><h2><span style="background-color:red">DELETE</span>: Delete a room use endpoint: /rooms/:id</h2></li>
+        <li><h2><span style="background-color:yellow">GET</span>: Get all Rooms use endpoint:<span style="background-color:yellow"> /rooms</span></h2></li>
+        <li><h2><span style="background-color:yellow">GET</span>: Take a list of Booked rooms use endpoint:<span style="background-color:yellow"> /rooms/customer</span></h2></li>
+        <li><h2><span style="background-color:green">POST</span>: Create a new room use endpoint:<span style="background-color:green"> /rooms/</span></h2></li>
+        <li><h2><span style="background-color:lightblue">PUT</span>: New Hall Booking use endpoint:<span style="background-color:lightblue"> /rooms/:id</span></h2></li>
+        <li><h2><span style="background-color:red">DELETE</span>: Delete a room use endpoint:<span style="background-color:red"> /rooms/:id</span></h2></li>
      </ul>
      </div>
      `);
